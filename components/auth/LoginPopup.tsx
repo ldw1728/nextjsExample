0d8abf67736f9008be7074911d4db3fe7b238bdf
zckixw11 <zckixw11@gmail.com>
2022-09-27 17:59:47 +0900

    update auth login

--- components/auth/LoginPopup.tsx
+++ components/auth/LoginPopup.tsx
@@ -3,15 +3,11 @@ import {useSession, signIn, signOut} from "next-auth/react"
 
 export default function LoginPopup({show}:any){
 
-
     return (
         <>
-        {
-            show ? <div className="LoginPopup">
+        <div className={show ? 'LoginPopup block' : 'LoginPopup hidden'}>
 
-            </div>: ''
-        }
-        
+        </div>
         </>
     )
 }
